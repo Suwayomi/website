cae5a9ae4928b880ada9f17b99711d2c8fc9c44d
Aria Moradi <aria.moradi007@gmail.com>
2023-01-07 22:16:28 +0330

    add markdown lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,30 +4,41 @@ module.exports = {
 		es6: true,
 	},
 
-	extends: ["vuepress", "prettier"],
-	plugins: ["vue", "prettier", "markdown"],
-
-	parserOptions: {
-		ecmaVersion: 2018,
-		parser: "@babel/eslint-parser",
-		requireConfigFile: false,
-		sourceType: "module",
-	},
-
-	rules: {
-		"prettier/prettier": [
-			"error",
-			{
-				endOfLine: "auto",
+	overrides: [
+		{
+			extends: ["vuepress", "prettier"],
+			plugins: ["vue", "prettier"],
+			files: ["*.js", "*.vue"],
+			parser: "@babel/eslint-parser",
+			parserOptions: {
+				ecmaVersion: 2018,
+				requireConfigFile: false,
+				sourceType: "module",
 			},
-		],
-		"import/no-unresolved": [
-			2,
-			{
-				ignore: ["^@"],
+			rules: {
+				"prettier/prettier": [
+					"error",
+					{
+						endOfLine: "auto",
+					},
+				],
+				"import/no-unresolved": [
+					2,
+					{
+						ignore: ["^@"],
+					},
+				],
+				curly: ["error", "all"],
+				"vue/multi-word-component-names": "off",
 			},
-		],
-		curly: ["error", "all"],
-		"vue/multi-word-component-names": "off",
-	},
+		},
+		{
+			files: ["*.md"],
+			parser: "eslint-plugin-markdownlint/parser",
+			extends: ["plugin:markdownlint/recommended"],
+			rules: {
+				"markdownlint/md025": ["error", { front_matter_title: "" }],
+			},
+		},
+	],
 };
