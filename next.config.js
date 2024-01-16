9fb4176cd1925db310ba5fdaf52851a510608c1e
Aria Moradi <aria.moradi007@gmail.com>
2024-01-16 16:16:46 +0330

    configure next for SSG build

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.tsx',
 });
   
-module.exports = withNextra();
+module.exports = withNextra({
+    // next config
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+});
