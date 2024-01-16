697ee8344b0410e6d123d7d49d30b8008e1bb95b
Aria Moradi <aria.moradi007@gmail.com>
2024-01-16 13:43:23 +0330

    add Next + Nextra

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>Suwayomi</span>,
+  project: {
+    link: 'https://github.com/Suwayomi',
+  },
+  chat: {
+    link: 'https://discord.gg/DDZdqZWaHA',
+  },
+  docsRepositoryBase: 'https://github.com/Suwayomi/website',
+  footer: {
+    text: 'Built with ❤️ using Nextra',
+  },
+};
+
+export default config;
